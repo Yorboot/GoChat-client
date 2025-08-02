99b9c9e29b57323e024d10e03c9e5adf021b3833
royboot <rgrizell467@gmail.com>
2025-08-03 01:16:09 +0200

    customized layout

--- GoChat/Views/MainView.axaml.cs
+++ GoChat/Views/MainView.axaml.cs
@@ -4,8 +4,19 @@ namespace GoChat.Views;
 
 public partial class MainView : UserControl
 {
+    string _message = string.Empty;
     public MainView()
     {
         InitializeComponent();
+        if (this.Message.Text != null || _message != string.Empty)
+        {
+            _message = Message.Text;
+        }
+        
+    }
+
+    public void SendData()
+    {
+        
     }
 }
