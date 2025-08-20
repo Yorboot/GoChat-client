ee86973e388f8089354ec853638a20581c3a9e95
royboot <rgrizell467@gmail.com>
2025-08-20 14:11:45 +0200

    implementation of niew view switcher

---
+++ GoChat/ViewModels/LoginViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GoChat.ViewModels;
+
+public class LoginViewModel:PageViewModelBase
+{
+    public string Title => "Login";
+
+    public override bool CanNavigateNext
+    {
+        get => true;
+        protected set
+        {
+            throw new NotSupportedException(); 
+        }
+    }
+
+    public override bool CanNavigatePrevious
+    {
+        get => false;
+        protected set => throw new NotSupportedException();
+    }
+}
