024c573afa890066f68343da2a49e0f81735b7cf
royboot <rgrizell467@gmail.com>
2025-08-21 13:40:59 +0200

    added MainWindowViewModel and start work on view navigation logic

---
+++ GoChat/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using ReactiveUI;
+
+namespace GoChat.ViewModels;
+
+public class MainWindowViewModel: ViewModelBase
+{
+    public MainWindowViewModel()
+    {
+        _currentPage = Pages[0];
+    }
+
+    private readonly PageViewModelBase[] Pages =
+    {
+        new MainViewModel(),
+        new LoginViewModel(),
+    };
+    private PageViewModelBase _currentPage;
+
+    public PageViewModelBase CurrentPage
+    {
+        get { return _currentPage; }
+        private set { this.RaiseAndSetIfChanged(ref _currentPage, value); }
+    }
+}
