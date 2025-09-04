904e2a588daa1e6e6562c9333f83f4cc6c1e8755
royboot <rgrizell467@gmail.com>
2025-09-04 10:53:04 +0200

    added abstract bools to controll where user can go

--- GoChat/ViewModels/PageViewModelBase.cs
+++ GoChat/ViewModels/PageViewModelBase.cs
@@ -2,7 +2,9 @@ using System.Runtime.CompilerServices;
 
 namespace GoChat.ViewModels;
 
-public class PageViewModelBase: ViewModelBase
+public abstract class PageViewModelBase: ViewModelBase
 {
-    
+    //Gets if the user can navigate forward or back
+    public abstract bool CanNavigateForward { get; protected set; }
+    public abstract bool CanNavigateBack { get; protected set; }
 }
