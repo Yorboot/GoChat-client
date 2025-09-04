758045d67b0a3f9ada02f9f9e449f25b3ae63219
royboot <rgrizell467@gmail.com>
2025-09-04 11:10:08 +0200

    added Inherit from PageViewModel base since its needed for Pages Array from MainViewModel.cs and added a singleton instance to stop multiple instances being made

--- GoChat/ViewModels/MainViewModel.cs
+++ GoChat/ViewModels/MainViewModel.cs
@@ -1,10 +1,13 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
 using ReactiveUI;
 
 namespace GoChat.ViewModels;
 
-public class MainViewModel:ViewModelBase
+public class MainViewModel:PageViewModelBase
 {
+    private static readonly Lazy<MainViewModel> _instance = new(() => new MainViewModel());
+    public static MainViewModel Instance => _instance.Value;
     public MainViewModel()
     {
     }
